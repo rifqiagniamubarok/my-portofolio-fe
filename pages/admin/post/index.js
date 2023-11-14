f1b21a7f21e7f8cc45c8f84babbccf5c006c896b
rifqi agnia mubarok <rifqiagniamubarok@gmail.com>
2023-11-14 09:07:47 +0700

    Feat: add image in list  post and button detail

--- pages/admin/post/index.js
+++ pages/admin/post/index.js
@@ -1,10 +1,44 @@
 import PostLayout from '@/components/Templates/PostLayout';
 import Link from 'next/link';
-import { AiFillDelete, AiFillEdit, AiFillFileAdd } from 'react-icons/ai';
+import { AiFillDelete, AiFillEdit, AiFillFile, AiFillFileAdd } from 'react-icons/ai';
 import { Button, Card, Input, Pagination, Switch, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow, Tooltip } from '@nextui-org/react';
 import { parse } from 'cookie';
+import axios from 'axios';
+import { useState } from 'react';
+import axiosInstance from '@/utils/axiosInstance';
+import Image from 'next/image';
 
-const Post = () => {
+const Post = ({ posts: data, page_info }) => {
+  const [posts, setPosts] = useState(data);
+  const [page, setPage] = useState(page_info.page);
+  const [pageSize, setPageSize] = useState(page_info.page_size);
+
+  const fetchData = async (page = 1) => {
+    try {
+      const {
+        data: {
+          data: { data, page_info },
+        },
+      } = await axiosInstance.get('/post', {
+        params: {
+          page: page,
+          page_size: 10,
+        },
+      });
+
+      setPosts(data);
+    } catch (error) {
+      console.error({ error });
+    }
+  };
+
+  const handlePagination = async (page) => {
+    try {
+      await fetchData(page);
+      setPage(page);
+    } catch (error) {}
+  };
+
   return (
     <PostLayout>
       <Card className="p-4 space-y-4">
@@ -20,34 +54,37 @@ const Post = () => {
           <Table removeWrapper aria-label="Example static collection table">
             <TableHeader>
               <TableColumn>No</TableColumn>
+              <TableColumn>THUMBNAIL</TableColumn>
               <TableColumn>NAME</TableColumn>
               <TableColumn className="hidden md:flex items-center">ABOUT</TableColumn>
-              <TableColumn></TableColumn>
+              <TableColumn>STATUS</TableColumn>
               <TableColumn></TableColumn>
             </TableHeader>
             <TableBody>
-              {[...Array(10)].map((_, index) => (
-                <TableRow key={index}>
-                  <TableCell>{index + 1}</TableCell>
-                  <TableCell>Tony Reichert</TableCell>
-                  <TableCell className="hidden md:inline-block">loremslkjfkldfjklj</TableCell>
+              {posts.map(({ id, thumbnail, slug, title, meta_description, is_publish }, index) => (
+                <TableRow key={id}>
+                  <TableCell>{index + 1 + (page - 1) * pageSize}</TableCell>
                   <TableCell>
-                    <Switch />
+                    <div className="h-10 aspect-video relative rounded-sm overflow-hidden">
+                      <Image src={thumbnail} alt={slug + '-image'} fill className="object-cover" />
+                    </div>
                   </TableCell>
-                  <TableCell className="space-x-2">
-                    <Button isIconOnly variant="faded" color="warning">
-                      <AiFillEdit />
-                    </Button>
-                    <Button isIconOnly variant="faded" color="danger">
-                      <AiFillDelete />
-                    </Button>
+                  <TableCell>{title}</TableCell>
+                  <TableCell className="hidden md:inline-block">{meta_description}</TableCell>
+                  <TableCell>{is_publish ? <p>publish</p> : <p>draft</p>}</TableCell>
+                  <TableCell>
+                    <Link href={`/admin/post/${id}`}>
+                      <Button isIconOnly variant="faded" color="primary">
+                        <AiFillFile />
+                      </Button>
+                    </Link>
                   </TableCell>
                 </TableRow>
               ))}
             </TableBody>
           </Table>
           <div className="flex justify-center mt-4 mb-4">
-            <Pagination total={20} initialPage={1} />
+            <Pagination total={page_info.total_pages} initialPage={1} page={page} onChange={handlePagination} />
           </div>
         </div>
       </Card>
@@ -67,9 +104,27 @@ export async function getServerSideProps(context) {
       },
     };
 
-  return {
-    props: { data: 'hello' },
-  };
+  try {
+    const {
+      data: {
+        data: { data, page_info },
+      },
+    } = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/post`, {
+      params: { page: 1, page_size: 10 },
+      headers: {
+        Authorization: `Bearer ${cookie.token}`,
+      },
+    });
+
+    const posts = data;
+    return {
+      props: { posts, page_info },
+    };
+  } catch (error) {
+    return {
+      props: { posts: [], page_info: {} },
+    };
+  }
 }
 
 export default Post;
