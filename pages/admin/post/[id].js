ab0ea6d99436c863984a86696144b9bcbe8741d5
rifqi agnia mubarok <rifqiagniamubarok@gmail.com>
2023-11-14 09:07:31 +0700

    Feat: detail post

---
+++ pages/admin/post/[id].js
@@ -0,0 +1,8 @@
+import PostLayout from '@/components/Templates/PostLayout';
+import React from 'react';
+
+const DetailPost = () => {
+  return <PostLayout>DetailPost</PostLayout>;
+};
+
+export default DetailPost;
