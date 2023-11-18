3012c09503395a7181cb44480189e8ba9e9a56ac
rifqi agnia mubarok <rifqiagniamubarok@gmail.com>
2023-11-19 00:17:41 +0700

    Fix: fix confit

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  publicRuntimeConfig: {
-    // Will be available on both server and client
-    backendUrl: process.env.BACKEND_URL,
-  },
+  publicRuntimeConfig: {},
   images: {
     domains: ['images.unsplash.com', 'res.cloudinary.com', 'localhost', 'api.rifqiagniamubarok.com'],
   },
