8311a252db20da369ab7164440169f4d74b5abfc
rifqi agnia mubarok <rifqiagniamubarok@gmail.com>
2023-11-23 08:37:18 +0700

    Feat: admin project

---
+++ pages/admin/project/index.js
@@ -0,0 +1,8 @@
+import ProjectLayout from '@/components/Templates/ProjectLayout';
+import React from 'react';
+
+const AdminProject = () => {
+  return <ProjectLayout>index</ProjectLayout>;
+};
+
+export default AdminProject;
---
+++ pages/admin/project/tech.js
@@ -0,0 +1,8 @@
+import ProjectLayout from '@/components/Templates/ProjectLayout';
+import React from 'react';
+
+const Tech = () => {
+  return <ProjectLayout>Tech</ProjectLayout>;
+};
+
+export default Tech;
