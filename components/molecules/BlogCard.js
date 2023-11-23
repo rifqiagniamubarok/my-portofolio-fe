8baf41653a9435f487d729b7581fc4f8a5e27ede
rifqi agnia mubarok <rifqiagniamubarok@gmail.com>
2023-11-23 08:35:23 +0700

    Fix: fix blog card

--- components/molecules/BlogCard.js
+++ components/molecules/BlogCard.js
@@ -14,17 +14,17 @@ const BlogCard = ({ thumbnail, title, tags, description = '', slug = '' }) => {
         <div className="relative w-full h-40  z-10 ">
           <div className="absolute bottom-2 right-2 flex z-20 gap-1">
             {tags.map((tag, index) => (
-              <Chip size="sm" key={index}>
+              <Chip size="sm" key={index} variant="faded" color="primary" className="">
                 {tag}
               </Chip>
             ))}
           </div>
           <Image src={thumbnail} alt="images" fill className="object-cover" />
         </div>
-        <div className=" p-4 z-20 bg-darkcard">
-          <p className="text-lg font-semibold">{title}</p>
-          <p className="text-base bg-gradient-to-r from-primary to-light-primary text-transparent bg-clip-text font-semibold">Nov 30, 2023</p>
-          <p className="text-sm text-justify text-gray-50">{displayDescription}</p>
+        <div className="p-4 z-20 bg-darkcard">
+          <p className="text-base lg:text-lg font-semibold">{title}</p>
+          <p className="text-sm lg:text-base bg-gradient-to-r from-primary to-light-primary text-transparent bg-clip-text font-semibold">Nov 30, 2023</p>
+          <p className="text-xs text-left text-gray-300">{displayDescription}</p>
         </div>
       </Card>
     </Link>
