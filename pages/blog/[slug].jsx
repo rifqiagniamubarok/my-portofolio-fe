ce83a290f38bf3d54ce482f710fc96d75e0f47e3
rifqi agnia mubarok <rifqiagniamubarok@gmail.com>
2023-11-23 08:35:37 +0700

    Fix: redesign blogs detail

--- pages/blog/[slug].jsx
+++ pages/blog/[slug].jsx
@@ -1,18 +1,23 @@
 import Article from '@/components/Organisms/Article';
 import BasicLayout from '@/components/Templates/BasicLayout';
-import { Divider } from '@nextui-org/react';
+import { Button, Chip, Divider } from '@nextui-org/react';
 import axios from 'axios';
 import classNames from 'classnames';
 import dayjs from 'dayjs';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import React, { useEffect, useState } from 'react';
+import { FaArrowLeft, FaEye } from 'react-icons/fa';
 
 const DetailBlog = ({ post: data, check }) => {
   const [post, setPost] = useState(data);
   const [activeHeading, setActiveHeading] = useState(null);
+  const router = useRouter();
+  const handleBack = () => {
+    router.back();
+  };
 
-  console.log({ check });
   // const parse = new DOMParser();
 
   // const doc = parse.parseFromString(post.body, 'text/html');
@@ -60,13 +65,31 @@ const DetailBlog = ({ post: data, check }) => {
       <section className="pt-20 space-y-4 dark">
         <div className="relative w-full h-32 md:h-60 overflow-hidden rounded-md" data-aos="fade-up" data-aos-duration="500">
           <Image src={post.thumbnail} alt={post.slug + '-image'} fill className="object-cover" />
+          <div className="absolute bottom-2 right-2 flex gap-1">
+            {post.tags.map(({ name }, index) => (
+              <Chip key={index}>{name}</Chip>
+            ))}
+          </div>
         </div>
         <div data-aos="fade-up" data-aos-duration="1000">
           <p className="text-2xl md:text-4xl font-semibold text-white">{post.title}</p>
           <p className="text-base md:text-base text-gray-400 ">Written on {dayjs(post.createdAt).format('MMM DD, YYYY')} by Rifqi Agnia M.</p>
-          <p className="text-base md:text-base text-gray-200 mt-2">{post.meta_description}</p>
+          {/* <p className="text-base md:text-base text-gray-200 mt-2">{post.meta_description}</p> */}
         </div>
-        <div data-aos="fade-up" data-aos-duration="1000">
+        <Link href={'/blog'} className="flex items-center gap-2" data-aos="fade-up" data-aos-duration="1100">
+          <Button variant="faded" color="primary" className="hidden lg:inline space-x-2 items-center">
+            <FaArrowLeft className="inline" />
+            <p className="inline">Back</p>
+          </Button>
+          <Button variant="faded" isIconOnly color="primary" className="lg:hidden">
+            <FaArrowLeft />
+          </Button>
+          <div className="flex items-center gap-2">
+            <FaEye className="fill-primary" />
+            <p className="text-primary text-base">{post.view}</p>
+          </div>
+        </Link>
+        <div data-aos="fade-up" data-aos-duration="1200">
           <div className="w-full h-0.5 bg-gradient-to-r from-primary to-light-primary"></div>
         </div>
         <div data-aos="fade-up" data-aos-duration="2000" className="grid grid-cols-1 md:grid-cols-4 gap-x-6 relative">
