e0bc3049152bdfbed9a55741bf1e9b2fff3299fb
rifqi agnia mubarok <rifqiagniamubarok@gmail.com>
2023-12-06 04:47:36 +0700

    Feat: image custom with handle error/fail

--- components/atoms/ImageCustom.js
+++ components/atoms/ImageCustom.js
@@ -1,16 +1,28 @@
 import { Skeleton } from '@nextui-org/react';
 import Image from 'next/image';
-import React, { useState } from 'react';
+import { useState } from 'react';
+import { MdOutlineImageNotSupported } from 'react-icons/md';
 
 const ImageCustom = ({ ...others }) => {
   const [isLoading, setIsLoading] = useState(true);
+  const [isFail, setIsFail] = useState(false);
   const handleLoading = (e) => {
     setIsLoading(false);
   };
 
+  const handleFail = (e) => {
+    setIsFail(true);
+  };
+
   return (
     <>
-      {isLoading && <Skeleton className="w-full h-full" />} <Image onLoad={handleLoading} {...others} />
+      {isFail && (
+        <div className="w-full h-full bg-gray-400 flex justify-center justify-center items-center">
+          <MdOutlineImageNotSupported />
+        </div>
+      )}
+      {isLoading && <Skeleton className="w-full h-full" />}
+      {!isFail && <Image onLoad={handleLoading} onError={handleFail} {...others} />}
     </>
   );
 };
