559c3cd9405db77c7ca8cb47712c44261693b306
rifqi agnia mubarok <rifqiagniamubarok@gmail.com>
2023-12-25 13:20:33 +0700

    Feat: project view

--- components/molecules/ProjectView.js
+++ components/molecules/ProjectView.js
@@ -1,10 +1,11 @@
 import { Button, Input } from '@nextui-org/react';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { AiFillPlusSquare, AiFillDelete, AiOutlineSend } from 'react-icons/ai';
 import { FaArrowUp, FaArrowDown } from 'react-icons/fa';
 
-const ProjectView = ({ onSubmit }) => {
+const ProjectView = ({ onChange }) => {
   const [formView, setFormView] = useState([{ name: '', url: '', position: 1 }]);
+  const [formViewFinal, setFormViewFinal] = useState([{ name: '', url: '', position: 1 }]);
   const formatForm = {
     name: '',
     url: '',
@@ -16,20 +17,20 @@ const ProjectView = ({ onSubmit }) => {
   };
 
   const handleNameChangeView = ({ target }) => {
-    let newFormView = formView;
+    let newFormView = [...formView];
     const { name, value } = target;
-    const getIndex = name.split('name-view-')[1];
+    const getIndex = Number(name.split('name-')[1]);
 
-    newFormView[getIndex].name = value;
+    newFormView[getIndex] = { ...newFormView[getIndex], name: value };
     setFormView(newFormView);
   };
 
-  const handleUrlChangeView = (index) => {
-    let newFormView = formView;
+  const handleUrlChangeView = ({ target }) => {
+    let newFormView = [...formView];
     const { name, value } = target;
-    const getIndex = name.split('url-view-')[1];
+    const getIndex = Number(name.split('url-')[1]);
 
-    newFormView[getIndex].url = value;
+    newFormView[getIndex] = { ...newFormView[getIndex], url: value };
     setFormView(newFormView);
   };
 
@@ -44,15 +45,32 @@ const ProjectView = ({ onSubmit }) => {
 
   const handlePositionUp = (index) => {
     if (index < 1) return;
-    let newFormView = formView;
-    newFormView[index].position = index;
-    newFormView[index - 1].position = index + 1;
+    let newFormView = [...formView];
+    newFormView[index] = { ...newFormView[index], position: index };
+    newFormView[index - 1] = { ...newFormView[index - 1], position: index + 1 };
     let getNew = newFormView.sort((a, b) => a.position - b.position);
+    setFormView(getNew);
+  };
+  const handlePositionDown = (index) => {
+    if (index >= formView.length - 1) return;
+    let newFormView = [...formView];
+    newFormView[index] = { ...newFormView[index], position: index + 2 };
+    newFormView[index + 1] = { ...newFormView[index + 1], position: index + 1 };
+
+    let getNew = newFormView.sort((a, b) => a.position - b.position);
+    setFormView(getNew);
   };
 
   const handleSubmitProjectView = () => {
     onSubmit(formView);
   };
+
+  useEffect(() => {
+    if (formView !== formViewFinal) {
+      setFormViewFinal(formView);
+      onChange(formView);
+    }
+  }, [formView, formViewFinal]);
 
   return (
     <div className="space-y-4">
@@ -69,24 +87,28 @@ const ProjectView = ({ onSubmit }) => {
           // .sort(function (a, b) {
           //   return a.position - b.position;
           // })
-          .map(({ name, url, position }, index) => (
-            <div className="flex gap-x-2 items-center" key={index}>
-              <Button isIconOnly variant="faded" disabled>
-                {position}
-              </Button>
-              <Input label="Name" size="sm" variant="faded" name={`name-view-${index}`} onChange={handleNameChangeView} />
-              <Input label="Url" size="sm" variant="faded" name={`url-view-${index}`} onChange={handleUrlChangeView} />
-              <Button isIconOnly variant="faded" onClick={() => handlePositionUp(index)}>
-                <FaArrowUp />
-              </Button>
-              <Button isIconOnly variant="faded">
-                <FaArrowDown />
-              </Button>
-              <Button isIconOnly variant="faded" color="danger" onClick={() => handleDeleteView(position)}>
-                <AiFillDelete />
-              </Button>
-            </div>
-          ))}
+          .map((view, index) => {
+            let { name, url, position } = view;
+
+            return (
+              <div className="flex gap-x-2 items-center" key={index}>
+                <Button isIconOnly variant="faded" disabled>
+                  {position}
+                </Button>
+                <Input label="Name" size="sm" variant="faded" onChange={handleNameChangeView} value={name} name={`name-${index}`} />
+                <Input label="Url" size="sm" variant="faded" name={`url-${index}`} value={url} onChange={handleUrlChangeView} />
+                <Button isIconOnly variant="faded" onClick={() => handlePositionUp(index)}>
+                  <FaArrowUp />
+                </Button>
+                <Button isIconOnly variant="faded" onClick={() => handlePositionDown(index)}>
+                  <FaArrowDown />
+                </Button>
+                <Button isIconOnly variant="faded" color="danger" onClick={() => handleDeleteView(position)}>
+                  <AiFillDelete />
+                </Button>
+              </div>
+            );
+          })}
       </div>
     </div>
   );
