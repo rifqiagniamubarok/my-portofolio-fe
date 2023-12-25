6fc26a3d30c6eb985f95db995e2d4c12660129a2
rifqi agnia mubarok <rifqiagniamubarok@gmail.com>
2023-12-25 13:20:23 +0700

    Feat: scal symbol (not done yet)

---
+++ components/atoms/ScaleSymbol.js
@@ -0,0 +1,14 @@
+import { Chip } from '@nextui-org/react';
+import React from 'react';
+
+const ScaleSymbol = () => {
+  return (
+    <>
+      <Chip variant="dot" color="primary" className="">
+        Minor
+      </Chip>
+    </>
+  );
+};
+
+export default ScaleSymbol;
